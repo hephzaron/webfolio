8f79d5332d1c9ad1e6e5b02acc09e38456115c22
hephzaron <hephzaron@users.noreply.github.com>
2025-09-19 00:56:46 +0000

    deploy: 64c0017b841f62510e7f2b8261a9443a47edac61

--- src/app/consts/skills.js
+++
@@ -1,6 +0,0 @@
-export default {
-    language: ["python", "js", "cpp", "verilog"],
-    other: ["dsp", "optimization"],
-    software: ["proteus", "ltspice", "matlab", "vivado"],
-    framework: ["scikit", "pandas", "numpy", "tensorflow", "keras", "pytorch", "opencv", "pyspark", "nltk"]  
-}
